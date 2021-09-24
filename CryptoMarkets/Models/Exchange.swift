d0eeeac575cf534a1964bf403208c5ec790ca61f
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-09-24 12:31:47 +0400

    MVC architecture

---
+++ CryptoMarkets/Models/Exchange.swift
@@ -0,0 +1,8 @@
+//
+//  Exchange.swift
+//  CryptoMarkets
+//
+//  Created by Nadzeya Shpakouskaya on 24/09/2021.
+//
+
+import Foundation
---
+++ CryptoMarkets/Models/Market.swift
@@ -0,0 +1,8 @@
+//
+//  Market.swift
+//  CryptoMarkets
+//
+//  Created by Nadzeya Shpakouskaya on 24/09/2021.
+//
+
+import Foundation
